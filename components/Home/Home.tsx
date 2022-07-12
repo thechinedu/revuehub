fef5917cf0758b78d063b91e4496d94fe90ce0d7
Chinedu Daniel <chinedudaniel7@gmail.com>
2022-07-12 09:36:06 +0200

    Homepage design #001

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,8 +1,10 @@
 import styles from "./Home.module.css";
+import landingIllustration from "@/public/pair-programming.svg";
 
 import type { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
@@ -20,8 +22,34 @@ const Home: NextPage = () => {
               Revue<span>Hub</span>
             </a>
           </Link>
+
+          <div>
+            <Link href="/sign-in">
+              <a>Sign in</a>
+            </Link>
+
+            <Link href="/sign-up">
+              <a>Sign up</a>
+            </Link>
+          </div>
         </nav>
       </header>
+
+      <main>
+        <section>
+          <Image src={landingIllustration} alt="" />
+          <h1>Review Github repositories without the need for pull requests</h1>
+          <p>
+            RevueHub provides line-by-line commenting, review and conversation
+            tools that enable easy and seamless collaboration between reviewers
+            and repository owners.
+          </p>
+
+          <Link href="/sign-up">
+            <a>Get started</a>
+          </Link>
+        </section>
+      </main>
     </div>
   );
 };
