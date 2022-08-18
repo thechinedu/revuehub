cd636d6f186e442670b6b158fa68b5b174093d39
Chinedu Daniel <chinedudaniel7@gmail.com>
2022-08-18 21:26:04 +0200

    Integrate static auth pages with API [status: incomplete, sn: 001] (#37)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
 import "modern-css-reset";
 import "@/styles/globals.css";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import type { AppProps } from "next/app";
 
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp;
