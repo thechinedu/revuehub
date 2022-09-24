68299523964fea4001a468061f42edd2ce2d659e
Chinedu Daniel <chinedudaniel7@gmail.com>
2022-09-24 09:29:52 +0200

    Add dashboard page [status: incomplete, sn: 001]

--- components/Dashboard/Dashboard.tsx
+++ components/Dashboard/Dashboard.tsx
@@ -20,14 +20,30 @@ const Dashboard: NextPage = () => {
         <SubNav className={styles.subNav}>
           {/* TODO: set active style only when page is the active view */}
           <Link href="/">
-            <a className={styles.active}>My Repositories</a>
+            <a className={styles.active}>My repositories (66)</a>
           </Link>
           <Link href="/">
-            <a>Review requested</a>
+            <a>Review requested (10)</a>
           </Link>
         </SubNav>
 
-        <main className={styles.main}></main>
+        <main className={styles.main}>
+          <a href="">
+            <section className={styles.repoSummary}>
+              <h3 className={styles.heading}>revuehub-api</h3>
+
+              <p className={styles.description}>
+                API for the revuehub platform - Review github repositories
+                without the need for pull requests
+              </p>
+
+              <div className={styles.meta}>
+                <p>Typescript</p>
+                <p>Updated 7 hours ago</p>
+              </div>
+            </section>
+          </a>
+        </main>
       </Container>
     </>
   );
