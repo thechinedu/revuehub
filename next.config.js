23d28606b87746caacc909d4f07eaeedca169895
Chinedu Daniel <chinedudaniel7@gmail.com>
2022-09-26 08:56:38 +0200

    Add dashboard page [status: incomplete, sn: 003]

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // TODO: Remove once integration with BE is complete
+  images: {
+    domains: ["placebeard.it"],
+  },
 };
 
 module.exports = nextConfig;
