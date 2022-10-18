26ff7e98e08a89cc2813117481ab49f8f6561919
Chinedu Daniel <chinedudaniel7@gmail.com>
2022-10-18 07:44:00 +0200

    Add E2E tests to CI pipeline

--- cypress/e2e/signup.cy.ts
+++ cypress/e2e/signup.cy.ts
@@ -1,15 +1,16 @@
-import { beforeEach, context, cy, describe, it } from "local-cypress";
+import { afterEach, beforeEach, cy, describe, it } from "local-cypress";
 
 describe("User signup flow", () => {
   beforeEach(() => {
+    cy.exec("yarn db:migrate");
+
     cy.visit("/");
     cy.get("[href='/sign-up']").first().click();
     cy.url().should("include", "/sign-up");
+  });
 
-    cy.get("#email").as("email").should("be.focused");
-    cy.get("#username").as("username");
-    cy.get("#password").as("password");
-    cy.get("[type='submit']").as("submit");
+  afterEach(() => {
+    cy.exec("yarn db:rollback");
   });
 
   it("loads the page correctly", () => {
@@ -18,12 +19,13 @@ describe("User signup flow", () => {
   });
 
   it("user signup is allowed when the provided credentials are correct", () => {
-    cy.get("@submit").should("be.disabled");
+    cy.get("[type='submit']").should("be.disabled");
 
-    cy.get("@email").type("test@example.com");
-    cy.get("@username").type("testy");
-    cy.get("@password").type("@mein passwort ist sup3r@");
+    cy.get("#email").should("be.focused").type("test@example.com");
+    cy.get("#username").type("testy");
+    cy.get("#password").type("@mein passwort ist sup3r@");
+    cy.get("[type='submit']").should("be.enabled").click();
 
-    cy.get("@submit").should("be.enabled");
+    cy.url().should("include", "/dashboard");
   });
 });
