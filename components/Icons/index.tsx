d67e0c7338b8faddff5a8413468d19ba5a9ccaa7
Chinedu Daniel <chinedudaniel7@gmail.com>
2022-11-28 11:51:27 +0100

    Implement static design for add a new repo page [status: incomplete, sn: 003]

--- components/Icons/index.tsx
+++ components/Icons/index.tsx
@@ -1,7 +1,8 @@
-export { Github as GithubIcon } from "./Github";
-export { At as AtIcon } from "./At";
-export { Envelope as EnvelopeIcon } from "./Envelope";
-export { Password as PasswordIcon } from "./Password";
 export { Account as AccountIcon } from "./Account";
+export { At as AtIcon } from "./At";
+export { Close as CloseIcon } from "./Close";
+export { Envelope as EnvelopeIcon } from "./Envelope";
 export { Eye as EyeIcon } from "./Eye";
 export { EyeSlash as EyeSlashIcon } from "./EyeSlash";
+export { Github as GithubIcon } from "./Github";
+export { Password as PasswordIcon } from "./Password";
