dee01533834bed931933f3fd7a8aa4cc0b261278
Chinedu Daniel <chinedudaniel7@gmail.com>
2022-12-05 09:58:29 +0100

    Implement static design for repo contents page [status: incomplete, sn: 001] (#155)

--- components/Icons/index.tsx
+++ components/Icons/index.tsx
@@ -4,5 +4,8 @@ export { Close as CloseIcon } from "./Close";
 export { Envelope as EnvelopeIcon } from "./Envelope";
 export { Eye as EyeIcon } from "./Eye";
 export { EyeSlash as EyeSlashIcon } from "./EyeSlash";
+export { FolderClosed as FolderClosedIcon } from "./FolderClosed";
+export { FolderOpened as FolderOpenedIcon } from "./FolderOpened";
+export { FolderTree as FolderTreeIcon } from "./FolderTree";
 export { Github as GithubIcon } from "./Github";
 export { Password as PasswordIcon } from "./Password";
